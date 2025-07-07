14650b6bfeaa2dc48958770661f4ea01753a91e8
Alex31122 <hector.alex.hernandez.villanueva@gmail.com>
2025-07-06 22:27:05 -0600

    feat: implementation of local Storage data saving

--- src/app/pages/home/components/add-todo/add-todo.ts
+++ src/app/pages/home/components/add-todo/add-todo.ts
@@ -1,27 +1,54 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';import { Component } from '@angular/core';
 import { ToDo } from '../../models/todoModel';
 
 @Component({
   selector: 'app-add-todo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-todo.html',
   styleUrl: './add-todo.css'
 })
 export class AddTodo {
   todo = new ToDo();
-  constructor(){
-    this.todo = this.addTodo("Make a commit", "Do a github commit to work remotely", "work");
-    console.log(this.todo);
-  }
-  
-  addTodo(name: string, description: string, type: string): ToDo {
-    const newTodo: ToDo = new ToDo( {name, description, type} );
-    // const id = crypto.randomUUID(); // Generar un ID único
-    // this.allTodos.push(newTodo);
-    // this.filteredTodos.push(newTodo);
+  todoListJson = localStorage.getItem('todoList');
+  todoList: ToDo[] = [];
+  listOptions = ["work", "home", "study"];
+  tagsListJson = localStorage.getItem('tagsList');
+  tagsList: string[] = [];
 
-    // this.todoMap.set(id, newTodo); // Añadir al mapa para acceso rápid
+  name = new FormControl('');
+  description = new FormControl('');
+  tag = new FormControl<string | null>("home");
+
+  constructor(){
+    // localStorage.removeItem('todoList');
+    // localStorage.removeItem('tagsList');
+    if(this.todoListJson){
+      this.todoList = JSON.parse(this.todoListJson);
+      console.log("To-Do List on local Storage");
+      console.log(this.todoList);
+    }
+
+    if(this.tagsListJson){
+      this.tagsList = JSON.parse(this.tagsListJson);
+      console.log("Tags List on local Storage");
+      console.log(this.tagsList);
+    }
+  }
+
+  addTodoToTodoList(name: string, description: string, type: string): ToDo {
+    const newTodo: ToDo = new ToDo();
+    newTodo.name = name;
+    newTodo.description = description;
+    newTodo.type = type;
+
+    this.todoList.push(newTodo);
+
+    this.addTodoListToLocalStorage();
     return newTodo;
   }
 
+  addTodoListToLocalStorage(){
+    const TodoList = JSON.stringify(this.todoList);
+    localStorage.setItem('todoList', TodoList);
+  }
 }
