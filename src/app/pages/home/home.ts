24e79621328f1f08cc30037aa9e17c54fb3683a6
Alex31122 <hector.alex.hernandez.villanueva@gmail.com>
2025-07-06 22:29:52 -0600

    refactor: separate the sections on components

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AllTasks } from "./components/all-tasks/all-tasks";
 import { AddTodo } from "./components/add-todo/add-todo";
+import { AddList } from "./components/add-list/add-list";
+import { LeftSection } from "./components/left-section/left-section";
 
 @Component({
   selector: 'app-home',
-  imports: [AllTasks, AddTodo],
+  imports: [AllTasks, AddTodo, AddList, LeftSection],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
