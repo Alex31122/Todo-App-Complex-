fd0abaebbbf2e57455d327d3e09eb1c72b1e55a6
Alex31122 <hector.alex.hernandez.villanueva@gmail.com>
2025-07-07 10:29:01 -0600

    feat: list the options on the left side

--- src/app/pages/home/components/left-section/left-section.ts
+++ src/app/pages/home/components/left-section/left-section.ts
@@ -1,11 +1,29 @@
-import { TagsList } from './../tags-list/tags-list';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, output, Output } from '@angular/core';
 @Component({
   selector: 'app-left-section',
-  imports: [TagsList],
+  imports: [],
   templateUrl: './left-section.html',
   styleUrl: './left-section.css'
 })
 export class LeftSection {
+  selection = output<string>();
+  tagsList: string[] = [];
+  tagsListJson = localStorage.getItem('tagsList');
+  constructor(){
+    if(this.tagsListJson){
+      this.tagsList = JSON.parse(this.tagsListJson);
+    }else{
+      this.tagsList.push("home");
+      this.tagsList.push("work");
+      this.addTagsListToLocalStorage();
+    }
+  }
+  addTagsListToLocalStorage(){
+    const tagsList = JSON.stringify(this.tagsList);
+    localStorage.setItem('tagsList', tagsList);
+  }
 
+  sendSelectionToHome(){
+    this.selection.emit("add_tag");
+  }
 }
