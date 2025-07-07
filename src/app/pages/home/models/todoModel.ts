4f11f6a99ba54416db02cfef68e59870c5844492
Alex31122 <hector.alex.hernandez.villanueva@gmail.com>
2025-07-06 22:28:08 -0600

    feat: added the is_completed property

--- src/app/pages/home/models/todoModel.ts
+++ src/app/pages/home/models/todoModel.ts
@@ -1,12 +1,16 @@
+import { FormControl } from "@angular/forms";
+
 export class ToDo{
     name: string;
     description: string;
     type: string;
+    is_completed: boolean;
 
     constructor(data?: Partial<ToDo>){
         this.name = data?.name ?? "Unnamed ToDo";
         this.description = "Undefined Description";
         this.type = data?.type ?? "Undefined Type";
+        this.is_completed = data?.is_completed ?? false;
     }
 
 }
